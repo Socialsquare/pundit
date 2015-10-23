b2dddfb74154a1cbe1fd34e9525bec166b881f76
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-10-23 20:16:15 +0200

    Enabling the use of a timeout - because the test was not failing.

--- test/init.js
+++ test/init.js
@@ -1,15 +1,21 @@
 var assert = require('assert');
-var Pundit = require('../pundit');
+var Pundit = require('../pundit').config('http://localhost:3000/');
 
 describe('Pundit', function() {
-  it('should be requireable and answer a simple question', function() {
+  it('should be requireable and answer a simple question', function(done) {
+    this.timeout(500);
 
     var q = 'What is the Answer to The Ultimate Question of Life, '+
             'the Universe, and Everything?';
-    var wisdom = Pundit.ask(q);
+    var knowledge = [
+      { args: [], returns: 42 }
+    ];
+
+    var wisdom = Pundit.ask(q, knowledge);
 
     wisdom().then(function(answer) {
       assert(answer === 42);
+      done();
     });
   });
 });
